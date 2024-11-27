d8d553ac7d80b6285906be55beb57b9def504a39
Keshav Mittal <work.keshavmittal9@gmail.com>
2024-11-28 00:13:31 +0530

    dictionary 20 ques

---
+++ coding-assignment/dict/dict_q02.py
@@ -0,0 +1,5 @@
+students = {
+    "Alice": {"age": 24, "grade": "A"},
+    "Bob": {"age": 22, "grade": "B"}
+}
+print(students["Alice"]["grade"])
---
+++ coding-assignment/dict/dicy_q01.py
@@ -0,0 +1,4 @@
+dict1 = {"x": 10, "y": 20}
+dict2 = {"y": 25, "z": 30}
+dict1.update(dict2)
+print(dict1)
